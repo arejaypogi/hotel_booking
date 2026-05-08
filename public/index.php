4acbca176541a8930249073786c3fe8e05429243
arejaypogi <arejay649@gmail.com>
2026-05-08 13:51:03 +0800

    Add index

---
+++ public/index.php
@@ -0,0 +1,21 @@
+<?php
+
+require_once '..core/Model.php';
+require_once '../core/Controller.php';
+
+// Default
+$url = isset($_GET['url']) ? $_GET['url'] : 'home/index';
+
+$url = explode('/', $url);
+
+$controllerName = ucfirst($url[0]) . "Controller";
+$method = $url[1] ?? 'index';
+
+require_once "../controllers/$controllerName.php";
+
+$controller = new $controllerName();
+
+call_userfunc_array([$controller, $method], []);
+
+
+
